451241231c9a0d54c0accea2bf339a7bcd8b2d2f
saqe <18554574+saqe@users.noreply.github.com>
2022-03-26 15:13:32 +0500

    Fake Database functions added.

--- database.py
+++ database.py
@@ -1,9 +1,21 @@
+from graphql_schema.type import User
+
 class FakeDatabase:
     def __init__(self):
-        self.data = []
+        self.__data = []
 
-    def get(self, email: str):
-        return {}
+    def get(self, email: str) -> User:
+        return next(self.__query(email))
 
-    def insert(self, user: dict):
-        return {}
+    def insert(self, user: User):
+        self.__data.append(user)
+        return user
+
+    def list(self):
+        return self.__data
+
+    def is_exist(self, email: str):
+        return any(self.__query(email))
+
+    def __query(self, email: str):
+        return filter(lambda user: user.email== email, self.__data)
