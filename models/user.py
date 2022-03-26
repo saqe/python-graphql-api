8b6cd0ff1b46a9f104d53318e5779c66fbb856cc
saqe <18554574+saqe@users.noreply.github.com>
2022-03-26 14:00:11 +0500

    Datetime coloumns added.

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
 from .database import Base
 
 class User(Base):
@@ -6,5 +6,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    hashed_password = Column(String, nullable= False)
+    is_active = Column(Boolean, nullable= False, default=True)
+    inserted_at = Column(DateTime, nullable= False, default=DateTime.utcnow)
+    last_logged_at = Column(DateTime)
