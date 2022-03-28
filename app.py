606685df3df0153f70c80839488db4d7f9e5a769
saqe <18554574+saqe@users.noreply.github.com>
2022-03-28 11:16:29 +0500

    Redirect host request to GraphQL endpoint

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from fastapi import FastAPI
 from strawberry.asgi import GraphQL
 from strawberry import Schema
 from graphql_schema import Query, Mutation
+from starlette.responses import RedirectResponse
 
 schema = Schema(query=Query, mutation=Mutation)
 
 graphql_app = GraphQL(schema)
 
 app = FastAPI()
+
+# Redirect Get root request to graphql endpoint
+@app.get("/")
+async def redirect():
+    return RedirectResponse(url='/graphql')
+
 app.add_route("/graphql", graphql_app)
 app.add_websocket_route("/graphql", graphql_app)
